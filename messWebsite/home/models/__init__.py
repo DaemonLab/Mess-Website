ff9c1e01e12562ce42089dc356a6b4fe62a79bf3
Mukul Jain <mukul.jain1906@yahoo.com>
2022-12-08 19:26:34 +0530

    Required changes

---
+++ messWebsite/home/models/__init__.py
@@ -0,0 +1,6 @@
+from .cafeteria import Cafeteria
+from .caterer import Caterer
+from .contacts import Contact
+from .home import About, Update
+from .links import Form
+from .rules import Rule, Penalty, ShortRebate, LongRebate
