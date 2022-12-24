9187758044df6a67350610beb1c12186308bee78
Ishaan Mittal <ishaanmittal123@gmail.com>
2022-12-24 12:09:10 +0530

    admin page customizing

--- messWebsite/home/models/home.py
+++ messWebsite/home/models/home.py
@@ -4,7 +4,7 @@ from django.utils.translation import gettext as _
 
 class About(models.Model):
     # Single About on Home Page
-    description = models.TextField(_("Description"))
+    description = models.TextField(_("Description"),help_text="The text added in this text field will show up in the about section of the home page.")
 
     def __str__(self):
         return "About Us Content"
@@ -16,11 +16,11 @@ class About(models.Model):
 
 class Update(models.Model):
     # All Updates on Home Page
-    update = models.CharField(_("update"), max_length=120)
-    time_stamp = models.DateTimeField(auto_now_add=True)
+    update = models.CharField(_("update"), max_length=120,help_text="The text added in this text field will show as one of the update in the update section")
+    time_stamp = models.DateTimeField(auto_now_add=True,help_text="time stamp of the update will also show up on the page and this also decides the order of the updates that show up")
 
     def __str__(self):
-        return "Posted on " + self.time_stamp
+        return "Posted on " + self.time_stamp.strftime("%m/%d/%Y, %H:%M:%S")
     
     class Meta:
         verbose_name = "Update"
