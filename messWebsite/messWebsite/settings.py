e881c4c61d846d4fa0a2935dfc0d5dc3724bc909
Ishaan Mittal <ishaanmittal123@gmail.com>
2023-04-16 20:02:51 +0530

    email showup on rebate

--- messWebsite/messWebsite/settings.py
+++ messWebsite/messWebsite/settings.py
@@ -136,7 +136,7 @@ AUTHENTICATION_BACKENDS = (
 
 SITE_ID = 3
 LOGIN_URL = "/accounts/google/login/"
-LOGIN_REDIRECT_URL = "/"
+LOGIN_REDIRECT_URL = "/rebateForm/"
 LOGIN_EXEMPT_URLS = ["/admin/*"]
 
 # Static files (CSS, JavaScript, Images)
