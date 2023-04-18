93adee12d8d04e4f1d0ad4af246c8149ab86143c
Ishaan Mittal <ishaanmittal123@gmail.com>
2023-04-19 02:45:25 +0530

    uploading full site FE database

--- messWebsite/home/models/caterer.py
+++ messWebsite/home/models/caterer.py
@@ -7,7 +7,7 @@ class Caterer(models.Model):
     name = models.CharField(_("Caterer Name"), max_length=50, help_text="The text in this text field contains the caterer name.")
     upper_description = models.TextField(_("Upper Description"), help_text="This contains the description of the respective caterer that will show on the upper side.")
     sheet_url = models.URLField(_("Menu URL"), help_text="This contains the google sheets url link for the respective caterers menu.")
-    lower_description = models.TextField(_("Lower Discription"), help_text="This contains the description of the respective caterer that will show on the lower side.")
+    lower_description = models.TextField(_("Lower Discription"),default="", help_text="This contains the description of the respective caterer that will show on the lower side.")
     student_limit = models.IntegerField(_("Caterers Student Limit"),default=0, help_text="The limit on number of students it can have")
     
     def __str__(self):
--- messWebsite/home/models/contacts.py
+++ messWebsite/home/models/contacts.py
@@ -4,7 +4,7 @@ from django.utils.translation import gettext as _
 
 class Contact(models.Model):
     # All Contacts on Contact Page
-    occupation = models.CharField(_("Occupation"), max_length=50,help_text="This contains the occupation of the contact to be added")
+    occupation = models.TextField(_("Occupation"), help_text="This contains the occupation of the contact to be added")
     hostel_sec = models.BooleanField(_("Dining Secretary"))
     name = models.CharField(_("Name"), max_length=30, null=True, blank=True,help_text="This contains the name of the contact to be added")
     contact = models.CharField(_("Phone Number"), max_length=10, null=True, blank=True,help_text="This contains phone number of the contact to be added")
