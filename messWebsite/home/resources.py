d3e702b8db7e321efbe59a0632f24402f193a38b
Ishaan Mittal <ishaanmittal123@gmail.com>
2023-04-18 20:06:10 +0530

    improvements

--- messWebsite/home/resources.py
+++ messWebsite/home/resources.py
@@ -15,35 +15,19 @@ class StudentResource(resources.ModelResource):
 
 class AllocationResource(resources.ModelResource):
     roll_no = ForeignKeyField(attribute='roll_no', column='roll_no')
-    
-    def get_export_headers(self):
-        headers = super().get_export_headers()
-        for i, h in enumerate(headers):
-            if h == 'roll_no':
-                headers[i] = "Roll No."
-            if h == 'roll_no__name':
-                headers[i] = "Name"
-            if h == 'roll_no__department':
-                headers[i] = "Department"
-            if h == 'roll_no__degree':
-                headers[i] = "Degree"
-            if h == 'roll_no__hostel':
-                headers[i] = "Hostel"
-            if h == 'roll_no__room_no':
-                headers[i] = "Room No."
-            if h == 'month':
-                headers[i] = "Month"
-            if h == 'student_id':
-                headers[i] = "Allocation ID"
-            if h == 'caterer_name':
-                headers[i] = "Caterer"
-            if h == 'high_tea':
-                headers[i] = "High Tea"
-            if h == 'first_pref':
-                headers[i] = "First Preference"
-            if h == 'second_pref':
-                headers[i] = "Second Preference"
-        return headers
+    roll_no = fields.Field(attribute="roll_no", column_name="Roll No.")
+    roll_no__name = fields.Field(attribute="roll_no__name", column_name="Name")
+    roll_no__department = fields.Field(attribute="roll_no__department", column_name="Department")
+    roll_no__degree = fields.Field(attribute="roll_no__degree", column_name="Degree")
+    roll_no__hostel = fields.Field(attribute="roll_no__hostel", column_name="Hostel")
+    roll_no__room_no = fields.Field(attribute="roll_no__room_no", column_name="Room No.")
+    month = fields.Field(attribute="month", column_name="Month")
+    student_id = fields.Field(attribute="student_id", column_name="Student ID")
+    caterer_name = fields.Field(attribute="caterer_name", column_name="Caterer Alloted")
+    high_tea = fields.Field(attribute="high_tea", column_name="High Tea")
+    first_pref = fields.Field(attribute="first_pref", column_name="First Preferences")
+    second_pref = fields.Field(attribute="second_pref", column_name="Second Preferences")
+    third_pref = fields.Field(attribute="third_pref", column_name="Third Preferences")
 
     class Meta:
         model = Allocation
@@ -88,6 +72,21 @@ class RebateResource(resources.ModelResource):
     #     column_name='approved'
     # )
 
+    email = fields.Field(attribute="email", column_name="Email")
+    allocation_id__roll_no__name = fields.Field(attribute="allocation_id__roll_no__name", column_name="Name")
+    allocation_id__roll_no__roll_no = fields.Field(attribute="allocation_id__roll_no__roll_no", column_name="Roll No.")
+    allocation_id__roll_no__department = fields.Field(attribute="allocation_id__roll_no__department", column_name="Department")
+    allocation_id__roll_no__degree = fields.Field(attribute="allocation_id__roll_no__degree", column_name="Degree")
+    allocation_id__roll_no__hostel = fields.Field(attribute="allocation_id__roll_no__hostel", column_name="Hostel")
+    allocation_id__roll_no__room_no = fields.Field(attribute="allocation_id__roll_no__room_no", column_name="Room No.")
+    allocation_id = fields.Field(attribute="allocation_id", column_name="Student ID")
+    allocation_id__caterer_name = fields.Field(attribute="allocation_id__caterer_name", column_name="Caterer Alloted")
+    allocation_id__high_tea = fields.Field(attribute="allocation_id__high_tea", column_name="High Tea")
+    date_applied = fields.Field(attribute="date_applied", column_name="date_applied")
+    start_date = fields.Field(attribute="start_date", column_name="Start Date")
+    end_date = fields.Field(attribute="end_date", column_name="End Date")
+    approved = fields.Field(attribute="approved", column_name="Approved")
+    
     class Meta:
         model = Rebate
         fields = (  "email",
@@ -97,8 +96,22 @@ class RebateResource(resources.ModelResource):
                     "allocation_id__roll_no__degree",
                     "allocation_id__roll_no__hostel",
                     "allocation_id__roll_no__room_no",
+                    "allocation_id__high_tea",
+                    "allocation_id__caterer_name",
                     "allocation_id",
                     "date_applied",
                     "start_date",
                     "end_date",
                     "approved",)
+        export_order = ["email",
+                        "allocation_id__roll_no__name",
+                        "allocation_id__roll_no__roll_no",
+                        "allocation_id__roll_no__department",
+                        "allocation_id__roll_no__degree",
+                        "allocation_id__roll_no__hostel",
+                        "allocation_id__roll_no__room_no",
+                        "allocation_id",
+                        "date_applied",
+                        "start_date",
+                        "end_date",
+                        "approved",]
