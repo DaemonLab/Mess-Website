2e9e9891e954cd4e2e9775c8e439052d2a8ada0d
Ishaan Mittal <ishaanmittal123@gmail.com>
2023-04-28 19:29:53 +0530

    restricting allauth urls

--- messWebsite/messWebsite/urls.py
+++ messWebsite/messWebsite/urls.py
@@ -18,9 +18,15 @@ from django.urls import path
 from django.urls import include
 from django.conf import settings
 from django.conf.urls.static import static
+from allauth.account.views import logout
+from allauth.socialaccount.providers.google.views import oauth2_login, oauth2_callback
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path('accounts/', include('allauth.urls')),
+    # path('accounts/', include('allauth.urls')),
+    path('accounts/logout/', logout, name="account_logout"),
+    # path('accounts/login/', login, name="account_logout"),
+    path('accounts/google/login/', oauth2_login, name="google_login"),
+    path('accounts/google/login/callback/', oauth2_callback, name="google_callback"),
     path("", include('home.urls')),
 ]+ static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
