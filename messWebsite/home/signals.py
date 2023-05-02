110e69ab9fefdc8b8707818da544fe9353e346b4
Ishaan Mittal <ishaanmittal123@gmail.com>
2023-05-02 22:30:46 +0530

    added signal.py

---
+++ messWebsite/home/signals.py
@@ -0,0 +1,97 @@
+from django.db.models.signals import post_save, pre_save
+from django.dispatch import receiver
+from .models import Student, RebateSpringSem,RebateAutumnSem, Rebate, Allocation
+from .views import count
+__doc__="This file contains the signals for the home app"
+
+@receiver(post_save, sender=Student)
+def create_bill(sender, instance, created, **kwargs):
+    if created:
+        email = instance.email
+        RebateAutumnSem.objects.get_or_create(email=email)
+        RebateSpringSem.objects.get_or_create(email=email)
+        instance.rebateautumnsem.save()
+        instance.rebatespringsem.save()
+
+
+def save_bill(email,month,days,high_tea):
+    # Add  in every if else block
+    print (email,month,days,high_tea)
+    if month == "January":
+        rebate = RebateSpringSem.objects.get(email=email)
+        rebate.highTeaJanuary = high_tea
+        rebate.januaryShort = rebate.januaryShort + days
+        rebate.save()
+    elif month == "February":
+        rebate = RebateSpringSem.objects.get(email=email)
+        rebate.highTeaFebruary = high_tea
+        rebate.februaryShort = rebate.februaryShort + days
+        rebate.save()
+    elif month == "March":
+        rebate = RebateSpringSem.objects.get(email=email)
+        rebate.highTeaMarch = high_tea
+        rebate.marchShort = rebate.marchShort + days
+        rebate.save()
+    elif month == "April":
+        rebate = RebateSpringSem.objects.get(email=email)
+        rebate.highTeaApril = high_tea  
+        rebate.aprilShort = rebate.aprilShort + days
+        rebate.save()
+    elif month == "May":
+        rebate = RebateSpringSem.objects.get(email=email)
+        rebate.highTeaMay = high_tea
+        rebate.mayShort = rebate.mayShort + days
+        rebate.save()
+    elif month == "June":
+        rebate = RebateSpringSem.objects.get(email=email)
+        rebate.highTeaJune = high_tea
+        rebate.juneShort = rebate.juneShort + days
+        rebate.save()
+    elif month == "July":
+        rebate = RebateAutumnSem.objects.get(email=email)
+        rebate.highTeaJuly = high_tea
+        rebate.julyShort = rebate.julyShort + days
+        rebate.save()
+    elif month == "August":
+        rebate = RebateAutumnSem.objects.get(email=email)
+        rebate.highTeaAugust = high_tea
+        rebate.augustShort = rebate.augustShort + days
+        rebate.save()
+    elif month == "September":
+        rebate = RebateAutumnSem.objects.get(email=email)
+        rebate.highTeaSeptember = high_tea
+        rebate.septemberShort = rebate.septemberShort + days
+        rebate.save()
+    elif month == "October":
+        rebate = RebateAutumnSem.objects.get(email=email)
+        rebate.highTeaOctober = high_tea
+        rebate.octoberShort = rebate.octoberShort + days
+        rebate.save()
+    elif month == "November":
+        rebate = RebateAutumnSem.objects.get(email=email)
+        rebate.highTeaNovember = high_tea
+        rebate.novemberShort = rebate.novemberShort + days
+        rebate.save()
+    elif month == "December":
+        rebate = RebateAutumnSem.objects.get(email=email)
+        rebate.highTeaDecember = high_tea
+        rebate.decemberShort = rebate.decemberShort + days
+        rebate.save()
+
+
+@receiver(pre_save, sender=Rebate)
+def update_bill(sender, instance, **kwargs):
+    print("Signal called")
+    try:
+        old_instance = Rebate.objects.get(pk=instance.pk)
+        print(old_instance.approved,instance.approved)
+        if old_instance.approved != instance.approved:
+            days = count(instance.start_date, instance.end_date)
+            email = instance.email
+            allocation = Rebate.objects.filter(allocation_id=instance.allocation_id).last().allocation_id
+            if instance.approved == True:
+                save_bill(email,allocation.month,days,allocation.high_tea)
+            else:
+                save_bill(email,allocation.month,-days,allocation.high_tea)
+    except Exception as e:
+        print(e)
