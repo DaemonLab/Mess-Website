9690d04e11c1e14105bc4f5e586de8c0636ccc23
Ishaan Mittal <ishaanmittal123@gmail.com>
2023-09-23 01:08:13 +0530

    high tea changes

--- home/utils/clean_rebate_sheet.py
+++ home/utils/clean_rebate_sheet.py
@@ -1,15 +1,15 @@
 import pandas as pd
 
-excel_file = r"C:\Users\ishaa\Downloads\2023-Spring-Hostel room allocation(15).xlsx"
-sheet_name = 'VSB'
+excel_file = r"C:\Users\ishaa\Downloads\Master Sheet Autumn 2023-Hostel room allocation.xlsx"
+sheet_name = 'HJB'
 
-column_to_check = 'Email'
+column_to_check = 'email'
 
 df = pd.read_excel(excel_file, sheet_name=sheet_name)
 
 df = df.dropna(subset=[column_to_check])
 
-output_file = r'C:\Users\ishaa\Downloads\vsb.xlsx'
+output_file = r'C:\Users\ishaa\Downloads\hjb.xlsx'
 
 df.to_excel(output_file, index=False)
 
