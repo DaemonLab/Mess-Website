a6f3b9003fe27fcd06538bc643b46263db676a86
Trishant Talluri <trishanttalluri@gmail.com>
2023-10-09 14:52:36 +0530

    Created a schedule to send rebate mails to caterer

---
+++ home/schedulers.py
@@ -0,0 +1,10 @@
+from django_apscheduler.jobstores import DjangoJobStore, register_events
+from apscheduler.schedulers.background import BackgroundScheduler
+from .utils.django_email_server import __send__rebate__email__
+
+def start():
+    scheduler = BackgroundScheduler()
+    scheduler.add_jobstore(DjangoJobStore(), "default")
+    scheduler.add_job(__send__rebate__email__, 'cron', hour=23, minute=59, second=0)
+    register_events(scheduler) 
+    scheduler.start()
--- home/utils/django_email_server.py
+++ home/utils/django_email_server.py
@@ -1,5 +1,5 @@
 # django-email-server.py
-
+from ..models.students import TodayRebate
 from django.core.mail import send_mail
 from django.core import mail
 from django.conf import settings
@@ -73,3 +73,30 @@ def long_rebate_mail(start_date, end_date, approved, recipient, left_start_date,
     else: 
         message = message_long_rebate.format(start_date=start_date, end_date=end_date, approved = "rejected",added="added to")
     send(subject, message, recipient)
+
+def __send__rebate__email__():
+    from datetime import date
+    today = date.today()
+    queryset = TodayRebate.objects.filter(date=today)
+    text = "<li> {name} with {allocation_id} has applied from {start_date} to {end_date}</li>"
+    messageKanaka=""
+    messageGauri=""
+    messageAjay=""
+    date = today
+    for obj in queryset:
+        allocation=obj.allocation_id
+        if(obj.Caterer == "Kanaka"):
+            messageKanaka +=(text.format(allocation_id=allocation.student_id,name=allocation.email.name, start_date=obj.start_date, end_date=obj.end_date))
+        elif(obj.Caterer == "Gauri"):
+            messageGauri +=(text.format(allocation_id=allocation.student_id,name=allocation.email.name, start_date=obj.start_date, end_date=obj.end_date))
+        elif(obj.Caterer == "Ajay"):
+            messageAjay +=(text.format(allocation_id=allocation.student_id,name=allocation.email.name, start_date=obj.start_date, end_date=obj.end_date))
+        obj.delete()
+    print(messageKanaka)
+    print(messageGauri)
+    print(messageAjay)
+    if(messageAjay): caterer_mail(messageAjay, "Ajay","mems220005054@iiti.ac.in", date)
+    if(messageKanaka): caterer_mail(messageKanaka, "Kanaka","mems220005054@iiti.ac.in",date)
+    if(messageGauri): caterer_mail(messageGauri, "Gauri","mems220005054@iiti.ac.in",date)
+    return
+
