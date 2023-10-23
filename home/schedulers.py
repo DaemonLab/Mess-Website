6d7c3c27fd309ce858723e876de8d48ed267b572
Ishaan Mittal <ishaanmittal123@gmail.com>
2023-10-23 23:13:16 +0530

    check

--- home/schedulers.py
+++ home/schedulers.py
@@ -1,18 +1,11 @@
 from django_apscheduler.jobstores import DjangoJobStore, register_events
 from apscheduler.schedulers.background import BackgroundScheduler
 from .utils.django_email_server import __send__rebate__email__
-import sys, socket
 
-try:
-    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    sock.bind(("127.0.0.1", 47200))
-except socket.error:
-    print, "!!!scheduler already started, DO NOTHING"
-else:
-    def start():
-        scheduler = BackgroundScheduler()
-        scheduler.add_jobstore(DjangoJobStore(), "default")
-        scheduler.add_job(__send__rebate__email__, 'cron', hour=23, minute=59, second=0)
-        register_events(scheduler) 
-        scheduler.start()
-        print, "scheduler started"
+def start():
+    scheduler = BackgroundScheduler()
+    scheduler.add_jobstore(DjangoJobStore(), "default")
+    scheduler.add_job(__send__rebate__email__, 'cron', hour=23, minute=59, second=0)
+    register_events(scheduler) 
+    scheduler.start()
+    print, "scheduler started"
