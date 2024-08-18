f7c6f3147d3c6c1fa75d817f289d33d9c4fc6da9
princekumarofficial <x.prince.x@outlook.com>
2024-08-18 22:13:16 +0530

    update settings.py

--- messWebsite/settings.py
+++ messWebsite/settings.py
@@ -4,7 +4,7 @@ from pathlib import Path
 import environ
 
 env = environ.Env()
-environ.Env.read_env()
+environ.Env.read_env(Path(__file__).resolve().parent.parent / '.env')
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
