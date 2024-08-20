23ce43b4e32f97d4f32439cc27cca79ae9cb9c55
Prince Kumar <x.prince.x@outlook.com>
2024-08-21 03:21:38 +0530

    Static files fix (#71)
    * Static files fix
    
    * update
    
    * update
    
    * update

--- messWebsite/settings.py
+++ messWebsite/settings.py
@@ -182,10 +182,10 @@ LOGIN_EXEMPT_URLS = ["/admin/*"]
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = "/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
 ]
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 # managing media
 CLOUDINARY_STORAGE = {
     "CLOUD_NAME": env("CLOUD_NAME"),
--- messWebsite/urls.py
+++ messWebsite/urls.py
@@ -20,9 +20,9 @@ Examples:
 
 from allauth.socialaccount.providers.google.views import oauth2_callback, oauth2_login
 from django.conf import settings
-from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
+from django.views.static import serve
 
 urlpatterns = [
     path("admin/doc/", include("django.contrib.admindocs.urls")),
@@ -31,4 +31,6 @@ urlpatterns = [
     path("accounts/google/login/", oauth2_login, name="google_login"),
     path("accounts/google/login/callback/", oauth2_callback, name="google_callback"),
     path("", include("home.urls")),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    path('media/<path:path>/', serve, {'document_root': settings.MEDIA_ROOT}),
+    path('static/<path:path>/', serve, {'document_root': settings.STATIC_ROOT}),
+]
