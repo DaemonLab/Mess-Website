299e3cac71a80ecfa28b5a00454e98d6c7620925
Ishaan Mittal <ishaanmittal123@gmail.com>
2024-11-17 12:18:11 +0530

    fix: is duplicate check logic (#99)

--- home/utils/rebate_checker.py
+++ home/utils/rebate_checker.py
@@ -14,55 +14,41 @@ def is_not_duplicate(student, new_rebate_start, new_rebate_end):
     try:
         for short_rebate in Rebate.objects.filter(email=student).all():
             if (
-                short_rebate.start_date - timedelta(days=2)
+                short_rebate.start_date
                 < new_rebate_start
-                < short_rebate.end_date + timedelta(days=1)
+                < short_rebate.end_date + timedelta(days=2)
             ) or (
                 short_rebate.start_date - timedelta(days=2)
                 < new_rebate_end
-                < short_rebate.end_date + timedelta(days=1)
+                < short_rebate.end_date
             ):
                 return False
         for short_rebate in LeftShortRebate.objects.filter(email=student).all():
             if (
-                short_rebate.start_date - timedelta(days=2)
+                short_rebate.start_date
                 < new_rebate_start
-                < short_rebate.end_date + timedelta(days=1)
+                < short_rebate.end_date + timedelta(days=2)
             ) or (
                 short_rebate.start_date - timedelta(days=2)
                 < new_rebate_end
-                < short_rebate.end_date + timedelta(days=1)
+                < short_rebate.end_date
             ):
                 return False
         for long_rebate in LongRebate.objects.filter(email=student).all():
             if (
-                long_rebate.end_date + timedelta(days=2)
+                long_rebate.end_date
                 > new_rebate_start
                 > long_rebate.start_date - timedelta(days=2)
             ) or (
                 long_rebate.start_date - timedelta(days=2)
                 < new_rebate_end
-                < long_rebate.end_date + timedelta(days=1)
+                < long_rebate.end_date
             ):
                 return False
         return True
     except Exception as e:
         print(e)
         return False
-
-
-# def is_present_rebate_bills(student, semester):
-#     """
-#     Checks if student is registered in the rebate bills,
-#     if not the function registers it with that email ID
-#     """
-#     try:
-#         student_bill = StudentBills.objects.get(email=student,semester=semester)
-#     except:
-#         print(Exception)
-#         student_bill = StudentBills(email=student,semester=semester)
-#         student_bill.save()
-#     return student_bill
 
 
 def max_days_rebate(student, start, end, period):
@@ -78,45 +64,31 @@ def max_days_rebate(student, start, end, period):
     match period.Sno:
         case 1:
             if student_bill.period1_short + sum <= 8:
-                # student_bill.period1_short+=sum
-                # student_bill.save(update_fields=["period1_short"])
                 return -1
             else:
                 return 8 - student_bill.period1_short
         case 2:
             if student_bill.period2_short + sum <= 8:
-                # student_bill.period2_short+=sum
-                # student_bill.save(update_fields=["period2_short"])
                 return -1
             else:
                 return 8 - student_bill.period2_short
         case 3:
             if student_bill.period3_short + sum <= 8:
-                # student_bill.period3_short+=sum
-                # student_bill.save(update_fields=["period3_short"])
                 return -1
             else:
                 return 8 - student_bill.period3_short
         case 4:
             if student_bill.period4_short + sum <= 8:
-                # student_bill.period4_short+=sum
-                # student_bill.save(update_fields=["period4_short"])
                 return -1
             else:
                 return 8 - student_bill.period4_short
         case 5:
             if student_bill.period5_short + sum <= 8:
-                # student_bill.period5_short+=sum
-                # student_bill.save(update_fields=["period5_short"])
                 return -1
             else:
                 return 8 - student_bill.period5_short
         case 6:
             if student_bill.period6_short + sum <= 8:
-                # student_bill.period6_short+=sum
-                # student_bill.save(update_fields=["period6_short"])
                 return -1
             else:
                 return 8 - student_bill.period6_short
-        # case default:
-        #     return -1
